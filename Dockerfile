4b813218f3e5e1876c88f85e0f863440bd9b2b34
c3h3 <c3h3.tw@gmail.com>
2014-11-15 04:39:15 +0800

    update Dockerfile and add local_settings.py

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,11 @@ MAINTAINER Chia-Chi Chang <c3h3.tw@gmail.com>
 
 RUN git clone https://github.com/c3h3/councilor-voter-guide.git /tmp/g0v-cvg && cd /tmp/g0v-cvg && git checkout 11a90d6e0eca89450654e0788f938b586f488484
 
+RUN apt-get update && apt-get install -y libpq-dev
+
+RUN cd /tmp/g0v-cvg/voter_guide && pip install -r requirements.txt
+
+
+
+
 
---
+++ local_settings.py
@@ -0,0 +1,19 @@
+
+import os        
+
+pgdb_uri = os.environ["POSTGRES_PORT"]
+pgdb_uri = pgdb_uri.replace("tcp://","")
+
+pgdb_host, pgdb_port = pgdb_uri.split(":")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'g0v',
+        'USER': 'postgres',
+        'HOST': pgdb_host,
+        'PORT': pgdb_port,
+    }
+}
+
+
